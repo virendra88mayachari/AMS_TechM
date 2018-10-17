b058cc3ff7d1ed45a1b760386e00f0360f9d7288
virendra88mayachari <44014539+virendra88mayachari@users.noreply.github.com>
2018-10-17 11:13:44 -0400

    Add files via upload

---
+++ AMSDemo/AMSDemo/Models/DIALS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMSDemo.Models
+{
+    public class DIALS
+    {
+        public int DialsID { get; set; }
+        public string TrackingNumber { get; set; }
+        public string ShiperNumber { get; set; }
+        public string ClarifiedSignature { get; set; }
+        public string ConsigneeName { get; set; }
+        public string Region { get; set; }
+        public string District { get; set; }
+        public string SLIC { get; set; }
+        public string Country { get; set; }
+        public string DeliverySLICState { get; set; }
+        public string City { get; set; }
+        public string StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string StreetType { get; set; }
+        public string BuildingFloorNumber { get; set; }
+        public string RoomSuiteNumber { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
