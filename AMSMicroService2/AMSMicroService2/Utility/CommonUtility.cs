b3e5622a1133bbac61aeeaeca90c7844f381e6f4
virendra88mayachari <44014539+virendra88mayachari@users.noreply.github.com>
2018-10-17 11:17:26 -0400

    Add files via upload

---
+++ AMSMicroService2/AMSMicroService2/Utility/CommonUtility.cs
@@ -0,0 +1,108 @@
+﻿using AMSMicroService2.Models;
+using Apache.NMS;
+using Apache.NMS.Util;
+using Newtonsoft.Json;
+using System;
+
+namespace AMSMicroService2.Utility
+{
+    public class CommonUtility
+    {
+        //public static void PushToActiveMQ(T opldObject, int queueNumber)
+        //{
+        //    Uri connecturi = new Uri("activemq:tcp://localhost:61616");
+
+        //    // NOTE: ensure the nmsprovider-activemq.config file exists in the executable folder.
+        //    IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+        //    using (IConnection connection = factory.CreateConnection())
+        //    using (ISession session = connection.CreateSession())
+        //    {
+        //        IDestination destination = SessionUtil.GetDestination(session, GETQueueName(queueNumber));
+
+        //        // Create the producer
+        //        using (IMessageProducer producer = session.CreateProducer(destination))
+        //        {
+        //            // Start the connection so that messages will be processed.
+        //            connection.Start();
+        //            producer.DeliveryMode = MsgDeliveryMode.Persistent;
+
+        //            var messageToQueue = JsonConvert.SerializeObject(opldObject);
+
+        //            //var ss = "Test Message " + DateTime.Now;
+        //            ITextMessage request = session.CreateTextMessage(messageToQueue);
+        //            request.NMSCorrelationID = "abc";
+        //            //request.Properties["NMSXGroupID"] = "cheese";
+        //            //request.Properties["myHeader"] = "Cheddar";
+
+        //            producer.Send(request);
+        //        }
+        //    }
+        //}
+
+        public static OPLD PullFromActiveMQ()
+        {
+            try
+            {
+                Uri connecturi = new Uri("activemq:tcp://54.173.238.145:61616");
+
+                // NOTE: ensure the nmsprovider-activemq.config file exists in the executable folder.
+                IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+                using (IConnection connection = factory.CreateConnection())
+                using (ISession session = connection.CreateSession())
+                {
+                    IDestination destination = SessionUtil.GetDestination(session, "queue://OPLDProcess");
+
+                    // Create a consumer and producer
+                    using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                    using (IMessageProducer producer = session.CreateProducer(destination))
+                    {
+                        // Start the connection so that messages will be processed.
+                        connection.Start();
+                        producer.DeliveryMode = MsgDeliveryMode.Persistent;
+                        
+                        // Consume a message
+                        ITextMessage message = consumer.Receive() as ITextMessage;
+                        if (message == null)
+                        {
+                            return default(OPLD);
+                        }
+                        else
+                        {
+                            OPLD opld = Newtonsoft.Json.JsonConvert.DeserializeObject<OPLD>(message.Text);
+
+                            return opld;
+                        }
+                    }
+                }
+            }
+            catch {
+                return default(OPLD);
+            }
+            
+        }
+
+        public static string GETQueueName(int queueNumber)
+        {
+            string queueName = string.Empty;
+
+            if (queueNumber == 1)
+            {
+                queueName = "queue://OPLDProcess";
+            }
+
+            if (queueNumber == 2)
+            {
+                queueName = "queue://CorrelationProcess";
+            }
+
+            if (queueNumber == 3)
+            {
+                queueName = "queue://ServicePointCreation";
+            }
+
+            return queueName;
+        }
+    }
+}
---
+++ AMSMicroService2/AMSMicroService2/Utility/ServicePointUtility.cs
@@ -0,0 +1,36 @@
+﻿using AMSMicroService2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMSMicroService2.Utility
+{
+    public class ServicePointUtility
+    {
+        public static ServicePoint CreateServicePoint(OPLD opldDetails,string consigneeName, string clarifiedSignature, bool isMatch)
+        {
+            //Create Service point
+            ServicePoint servicePoint = new ServicePoint();
+            servicePoint.AddressLine1 = opldDetails.SHIP_ADDR_LINE_1_1;
+            servicePoint.AddressLine2 = opldDetails.SHIP_ADDR_LINE_1_2;
+            servicePoint.AddressLine3 = opldDetails.SHIP_ADDR_LINE_1_3;
+            servicePoint.AddressType = opldDetails.SHIP_ADDR_TYPE_1;
+            servicePoint.AttentionName = opldDetails.SHIP_ATTENTION_NAME_1;
+            servicePoint.CityName = opldDetails.SHIP_CITY_NAME_1;
+            servicePoint.CountryCode = opldDetails.SHIP_COUNTRY_CODE_1;
+            servicePoint.CreatedDate = System.DateTime.Now.ToString();
+            servicePoint.PhoneNumber = opldDetails.SHIP_PH_NR_1;
+            servicePoint.ShiperNumber = opldDetails.PAGE_SHIPPER_NUMBER;
+            servicePoint.StateCode = opldDetails.SHIP_STATE_CODE_1;
+            servicePoint.ZipCode = opldDetails.SHIP_ZIP_CODE_1;
+
+            servicePoint.ConsigneeName = string.IsNullOrEmpty(consigneeName) ? opldDetails.SHIP_ATTENTION_NAME_1 : consigneeName;
+            servicePoint.SignatureClarify = clarifiedSignature;
+
+            servicePoint.ServicePointStatus = (isMatch ? "Match found between OPld and DIALS" : "No Match found between OPld and DIALS");
+
+            return servicePoint;
+        }
+    }
+}
